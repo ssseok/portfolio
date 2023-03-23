6f17a09c2bd8c6434e405deaaa25469494517c80
Hyun Seok Park <luckseok1@gmail.com>
2023-03-23 22:41:00 +0900

    feat: lottie animation 적용

---
+++ components/home/hero.tsx
@@ -0,0 +1,27 @@
+import Animation from "./animation";
+
+export default function Hero() {
+  return (
+    <>
+      <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+          선한 영향력을 주는 개발자 박현석입니다.
+          <br className="hidden lg:inline-block" />
+          화이팅!
+        </h1>
+        <p className="mb-8 leading-relaxed">
+          선한 영향력을 주는 개발자 박현석입니다.
+        </p>
+        <div className="flex justify-center">
+          <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            프로젝트 보러가기
+          </button>
+        </div>
+      </div>
+
+      <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <Animation />
+      </div>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Layout from "@/components/layout";
+import Hero from "@/components/home/hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,22 +17,7 @@ export default function Home() {
       </Head>
       <section className=" flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              선한 영향력을 주는 개발자 박현석입니다.
-              <br className="hidden lg:inline-block" />
-              화이팅!
-            </h1>
-            <p className="mb-8 leading-relaxed">
-              선한 영향력을 주는 개발자 박현석입니다.
-            </p>
-            <div className="flex justify-center">
-              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                프로젝트 보러가기
-              </button>
-            </div>
-          </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6"></div>
+          <Hero />
         </div>
       </section>
     </Layout>
